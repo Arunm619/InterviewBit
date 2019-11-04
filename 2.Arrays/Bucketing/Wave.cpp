2036f28d73927c9d44112b31bfcadb7db30d9eb5
arun-sudharsan <arun.sudharsan@freshworks.com>
2019-11-04 19:18:23 +0530

    Added Wave array

--- 2.Arrays/Bucketing/Wave.cpp
+++ 2.Arrays/Bucketing/Wave.cpp
@@ -22,4 +22,25 @@ One possible answer : [2, 1, 4, 3]
 Another possible answer : [4, 1, 3, 2]
  NOTE : If there are multiple answers possible, return the one thats lexicographically smallest.
 So, in example case, you will return [2, 1, 4, 3]
+
+ Since it is asked to find lexicographically smaller sequence, we cannot do better than nlogn otherwise we can do in linear time.
+
+
+vector<int> Solution::wave(vector<int> &A) {
+    bool flag = true;
+    for(int i= 0; i < A.size()-1; i++)
+    {
+        if(flag){
+            if(A[i]<A[i+1])
+                swap(A[i],A[i+1]);
+        }
+        else{
+            if(A[i]>A[i+1])
+                swap(A[i],A[i+1]);
+        }
+        flag = !flag;
+    }
+    return A;
+}
+
  */
